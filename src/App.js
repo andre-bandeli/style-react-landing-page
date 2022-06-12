72d15a32b24690a5909663b65702b4ae5028b271
andre-bandeli <andre.lbandeli@gmail.com>
2022-06-12 02:54:50 -0300

    feat: navbar responsive bootstrap react

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
 import Projects from './components/projects/Projects';
 import Team from './components/team/Team';
 
 function App() {
   return (
     <div className="App">
+      
+      <Navbar/>
       <Header/>
       <About/>
       <Team/>
